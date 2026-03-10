a83d86edec913d2425c880205b7fcad9a6ec1565
kawan santana <uchihakawan38@gmail.com>
2026-03-09 22:04:26 -0300

    Ex do João

---
+++ Exercioa_João/EX2.js
@@ -0,0 +1,13 @@
+let a = 10;
+let b = 6;
+
+function somar(a,b) {
+}
+ 
+console.log(a + b);
+console.log(a - b); 
+console.log(a * b)
+console.log(a / b)
+console.log(a % b)
+
+    return a - b;
---
+++ Exercioa_João/EX3.js
@@ -0,0 +1,7 @@
+let numero = 4; 
+
+if (numero % 2 == 0){
+    console.log("Este  numero é par")
+} else {
+        console.log("Este numero é impar");
+}
