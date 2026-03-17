b152bfb19d5acfdd30958233826ad39b433b2318
kawan santana <uchihakawan38@gmail.com>
2026-03-16 22:11:02 -0300

    salvo

---
+++ EX4.js
@@ -0,0 +1,8 @@
+let idade = 14;
+
+if
+(idade <18){
+    console.log("Vc é menor de idade");
+}else{
+            console.log("Você é de maior");
+}
---
+++ EX7.js
@@ -0,0 +1,22 @@
+let numeros = [2,3,4];
+
+// let num1 = [0]
+// let num2 = [1]
+// let num3 = [2]
+
+console.log(numeros[0]  + numeros[1] + numeros[2]);
+
+
+
+// let num1 = 10
+// let num2 = 20
+
+// console.log(num1 + num2)
+
+
+// function somar ( num ){
+//     return num > ;
+// }
+
+
+// console.log(somar (numeros));
